2295b98393a083ff5ed8f4eae28f46f5893c5c2a
Galaceon <antoniogarcialtr@gmail.com>
2026-04-04 07:07:26 +0200

    using localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
 import { cartReducer, initialState } from "./reducers/cart-reducer"
 
 function App() {
 
-
     const [state, dispatch] = useReducer(cartReducer, initialState)
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(state?.cart))
+    }, [state?.cart])
 
     return (
         <>
             <Header 
-                cart={state?.cart}
+                cart={state!.cart}
                 dispatch={dispatch}
             />
 
